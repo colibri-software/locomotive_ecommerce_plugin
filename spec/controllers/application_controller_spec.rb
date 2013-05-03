f775a59ed520d870fcd0868c875e5cc5fa92315f
Daniel <daniel.deveau@colibri-software.com>
2013-05-03 11:15:35 -0300

    Wrote ApplicationController#authenticate_user! spec.

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+module Ecommerce
+  describe ApplicationController do
+    context '#authenticate_user!' do
+      context 'valid user' do
+        controller(::Ecommerce::ApplicationController) do
+          before_filter :authenticate_user!
+
+          def index 
+            render :text => 'Test!'
+          end
+        end
+
+        it 'does nothing' do
+          controller.stub(:current_user).and_return(true)
+          get :index
+          response.body.should include('Test')
+        end
+      end
+
+      context 'invalid user' do
+        controller(::Ecommerce::ApplicationController) do
+          before_filter :authenticate_user!
+
+          def index
+            render :text => 'Test!'
+          end
+        end
+
+        it 'prevents viewing the page' do
+          controller.stub(:current_user).and_return(nil)
+          controller.should_receive(:redirect_to)
+          get :index
+        end
+      end
+    end
+  end
+end
