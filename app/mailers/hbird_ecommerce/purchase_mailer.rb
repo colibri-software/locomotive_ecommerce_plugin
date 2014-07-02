6f59e9827c1279adf7d6b08d71da7da7815219fa
Charlie Greene <greeneca@gmail.com>
2014-07-02 10:29:31 -0300

    update from address on mailer

--- app/mailers/hbird_ecommerce/purchase_mailer.rb
+++ app/mailers/hbird_ecommerce/purchase_mailer.rb
@@ -1,6 +1,6 @@
 module HbirdEcommerce
   class PurchaseMailer < ActionMailer::Base
-    default from: "from@example.com"
+    default from: Engine.config_or_default('contact')
   
     # Subject can be set in your I18n file at config/locales/en.yml
     # with the following lookup:
