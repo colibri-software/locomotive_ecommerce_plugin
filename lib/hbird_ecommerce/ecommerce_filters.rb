adefb36d8c84255ce148b08d51963e29fa60c3c7
Charlie Greene <greeneca@gmail.com>
2014-10-02 12:39:15 -0300

    add filter to calculate shipping

--- lib/hbird_ecommerce/ecommerce_filters.rb
+++ lib/hbird_ecommerce/ecommerce_filters.rb
@@ -29,5 +29,27 @@ module HbirdEcommerce
     def find_purchase(id)
       Purchase.find(id.to_s)
     end
+
+    def calculate_shipping(method_name)
+      session = @context.registers[:controller].session
+      user = IdentityPlugin::User.where(id: session[:user_id]).first
+      id = user == nil ? nil : user.id
+      if id
+        cart = Cart.find_or_create(id, session)
+        ct = Thread.current[:site].content_types.where(slug: Engine.config_or_default('shipping_model')).first
+        price_break = Engine.config_or_default('price_break').to_f
+        name_field = Engine.config_or_default('shipping_name_slug').to_sym
+        over_field = Engine.config_or_default('shipping_over_slug').to_sym
+        under_field = Engine.config_or_default('shipping_under_slug').to_sym
+        query_hash = {}
+        query_hash[name_field] = method_name
+        method = ct.entries.where(query_hash).first
+        if method && cart.purchase_total > price_break
+          method.send(over_field).to_f
+        elsif method
+          method.send(under_field).to_f
+        end
+      end
+    end
   end
 end
--- lib/hbird_ecommerce/version.rb
+++ lib/hbird_ecommerce/version.rb
@@ -1,3 +1,3 @@
 module HbirdEcommerce
-  VERSION = "1.2.3ts"
+  VERSION = "1.3.0ts"
 end
